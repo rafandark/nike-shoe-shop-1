4560cfca90630dc50e6f00ea3169e349d5aadb6c
rafandark <90086345+rafandark@users.noreply.github.com>
2023-08-26 12:03:02 +0600

    cartitems

--- components/CardItem.jsx
+++ components/CardItem.jsx
@@ -1,105 +1,120 @@
-import React from 'react'
-import {RiDeleteBinGLine} from 'react-icons/ri'
-import { easeInOut, motion, useMotionValue } from 'framer-motion'
-import Image from 'next/image'
+import Image from "next/image";
+import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { updateCart, removeFromCart } from "../store/cartSlice";
+import { useDispatch } from "react-redux";
+const CartItem = ({ data }) => {
+    const p = data.attributes;
 
-const CardItem = ({data}) => {
+    const dispatch = useDispatch();
 
-  
-  const p = data?.attributes 
+    const updateCartItem = (e, key) => {
+        let payload = {
+            key,
+            val: key === "quantity" ? parseInt(e.target.value) : e.target.value,
+            id: data.id,
+        };
+        dispatch(updateCart(payload));
+    };
 
-  return (
-    <motion.div
-    initial={{y:200}}
-    whileInView={{y:0 , transition:{duration:0.5 , ease:easeInOut}}}
-    
- 
-    
-    className='flex my-5 gap-3 md:gap-5 border-b border-cyan-300'>
-        {/* Image  start*/}
-        <div className='shirnk-0 aspect-square w-[50px]  md:w-[100px]'>
-           <Image width={120}
-           height={120} className='rounded-lg' src={p?.thumbnail?.data?.attributes?.url} alt={p?.name}/>
+    return (
+        <div className="flex py-5 gap-3 md:gap-5 border-b">
+            {/* IMAGE START */}
+            <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
+                <Image
+                    src={p.thumbnail.data.attributes.url}
+                    alt={p.name}
+                    width={120}
+                    height={120}
+                />
+            </div>
+            {/* IMAGE END */}
 
-          
+            <div className="w-full flex flex-col">
+                <div className="flex flex-col md:flex-row justify-between">
+                    {/* PRODUCT TITLE */}
+                    <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
+                        {p.name}
+                    </div>
+
+                    {/* PRODUCT SUBTITLE */}
+                    <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
+                        {p.subtitle}
+                    </div>
+
+                    {/* PRODUCT PRICE */}
+                    <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
+                        MRP : &#8377;{p.price}
+                    </div>
+                </div>
+
+                {/* PRODUCT SUBTITLE */}
+                <div className="text-md font-medium text-black/[0.5] hidden md:block">
+                    {p.subtitle}
+                </div>
+
+                <div className="flex items-center justify-between mt-4">
+                    <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
+                        <div className="flex items-center gap-1">
+                            <div className="font-semibold">Size:</div>
+                            <select
+                                className="hover:text-black"
+                                onChange={(e) =>
+                                    updateCartItem(e, "selectedSize")
+                                }
+                            >
+                                {p.size.data.map((item, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={item.size}
+                                            disabled={
+                                                !item.enabled ? true : false
+                                            }
+                                            selected={
+                                                data.selectedSize === item.size
+                                            }
+                                        >
+                                            {item.size}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+
+                        <div className="flex items-center gap-1">
+                            <div className="font-semibold">Quantity:</div>
+                            <select
+                                className="hover:text-black"
+                                onChange={(e) => updateCartItem(e, "quantity")}
+                            >
+                                {Array.from(
+                                    { length: 10 },
+                                    (_, i) => i + 1
+                                ).map((q, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={q}
+                                            selected={data.quantity === q}
+                                        >
+                                            {q}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                    </div>
+                    <RiDeleteBin6Line
+                        onClick={() =>
+                            dispatch(removeFromCart({ id: data.id }))
+                        }
+                        className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]"
+                    />
+                </div>
+            </div>
         </div>
-        {/* iamge end */}
-        <div className='w-full flex flex-col'>
-    <div className='flex flex-col md:flex-row justify-between '>
-          {/* title  start*/}
-          <div className='text-lg md:text-xl font-semibold text-black/[0.9]'
-          > {p?.name}</div>
-            {/* title end*/}
+    );
+};
 
-            {/* sub title start*/}
-            <div className='md:hidden text-sm font-medium text-black/[0.5]'>
-               {p?.subtitle}
-            </div>
-
-            {/* sub title start*/}
-              {/*price start  */}
-            <div className='text-sm md:text-md font-bold text-black/[0.5]'>
-          MRP :&#8377;{p?.price }
-            </div>
-                          {/*price end  */}
-                 
-
-         </div>
-         <div className=' text-sm font-medium text-black/[0.5]'>
-          {p?.subtitle}
-
-            </div>
-
-            {/* seletor s*/}
-           <div className='flex items-center justify-between mt-4'>
-            <div className='flex items-center gap-2 md:gap-10 text-black-[0.5]'>
-         <div className='flex items-center gap-1'>
-         <div className='font-semibold opacity-70'>size</div>
-          <select className='hover:bg-black/[0.5]' name="" id="">
-            <option value="1">UK 6</option>
-            <option value="1">UK 6.5</option>
-            <option value="1">UK 7</option>
-            <option value="1">UK 7.5</option>
-            <option value="1">UK 8</option>
-            <option value="1">UK 8.5</option>
-            <option value="1">UK 9</option>
-            <option value="1">UK 9.5</option>
-            <option value="1">UK 10</option>
-          </select>
-            </div>
-  {/* option value s*/}
-  <div className='flex items-center gap-1'>
-            Quantity
-            <div>
-            <select className='hover:bg-black/[0.5]' name="" id="">
-            <option value="1">UK 6</option>
-            <option value="1">UK 6.5</option>
-            <option value="1">UK 7</option>
-            <option value="1">UK 7.5</option>
-            <option value="1">UK 8</option>
-            <option value="1">UK 8.5</option>
-            <option value="1">UK 9</option>
-            <option value="1">UK 9.5</option>
-            <option value="1">UK 10</option>
-          </select>
-            </div>
-           </div>
-           {/* option value s*/}
-         </div>
-
-          
-
-
-           </div>
-
-           
-          {/* seletor s*/}
-
-
-        </div>
-        
-    </motion.div>
-  )
-}
-
-export default CardItem
+export default CartItem;
